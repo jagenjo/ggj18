fdc6ba135d75f0b760f8b91236adfc27d8e5615f
Mik-C <miquelclark@hotmail.com>
2018-01-27 19:27:37 +0100

    outrun road

--- js/game_outrun.js
+++ js/game_outrun.js
@@ -1,8 +1,9 @@
 
 var GameOutrun = {
 	name: "outrun",
+	version: "miquel1",
 	scale: 1,
-	to_load: [], //urls of images and sounds to load
+	to_load: ["data/outrun/ball8.png", "data/outrun/boxgear.png", "data/outrun/car.png", "data/outrun/fondo.png", "data/outrun/police.png"], //urls of images and sounds to load
 	
 	//ALL GAME STATE SHOULD BE HERE, DO NOT STORE WEIRD STUFF LIKE IMAGES, DOM, ETC
 	//GAME STATE IS SENT TO SERVER EVERY FRAME so keep it light
@@ -10,12 +11,23 @@ var GameOutrun = {
 		time: 0, //time since game started
 		mousedown: false,
 		mousedown_was_pressed: false,
-		blips: [],
+
+		car: [],
 	},
 
 	//called after loading all games, init stuff here
 	onInit: function()
 	{
+	},
+
+	onEneter: function()
+	{
+
+	},
+
+	onLeave: function()
+	{
+
 	},
 	
 	//called when the game starts
@@ -26,20 +38,47 @@ var GameOutrun = {
 		//reset game state
 		this.state.time = 0;
 	},
+
+	drawRoad: function( ctx, ymin, ymax )
+	{
+		var y = ymin;
+		var colors = ["#555555", "#888888"];
+		var t = this.state.time;
+		var v = 12;
+		for(var i=0; y < ymax; i++){
+			var h = Math.floor(Math.pow(i+(v*t) % 1, 1.2));
+
+			ctx.fillStyle = colors[Math.floor(v*t + i)%2];
+			ctx.fillRect(0, y, 300, h);
+			y+= h;
+		}
+
+		ctx.fillStyle = "#81c137";
+		ctx.beginPath();
+		ctx.moveTo(0, ymin-1);
+		ctx.lineTo(70, ymin);
+		ctx.lineTo(0, ymax);
+		ctx.fill();
+		ctx.beginPath();
+		ctx.moveTo(ctx.canvas.width, ymin-1);
+		ctx.lineTo(ctx.canvas.width - 70, ymin);
+		ctx.lineTo(ctx.canvas.width, ymax);
+		ctx.fill();
+	},
 	
 	//render one frame, DO NOT MODIFY STATE
 	onRender: function( canvas )
 	{
 		var ctx = canvas.getContext("2d");
-		ctx.fillStyle = "black";
-		ctx.fillRect( 0,0, canvas.width, canvas.height );
-		ctx.strokeStyle = "cyan";
-		for(var i = 0; i < this.state.blips.length; ++i)
-		{
-			var blip = 	this.state.blips[i];
-			ctx.circle( blip.x, blip.y, (this.state.time - blip.time) * 10 );
-			ctx.stroke();
-		}
+		ctx.drawImage( APP.assets["data/outrun/fondo.png"], 0, 0 );
+		ctx.save();
+		ctx.beginPath();
+		ctx.rect(4, 60, 246, 121);
+		ctx.clip();
+		this.drawRoad(ctx, 100, 224);
+		ctx.drawImage( APP.assets["data/outrun/police.png"], 100, 100 );
+		ctx.drawImage( APP.assets["data/outrun/car.png"], 80, 140 );
+		ctx.restore();
 	},
 
 	//update game state	
@@ -50,8 +89,6 @@ var GameOutrun = {
 	
 	onMouse: function( e )
 	{
-		if(e.type == "mousedown" )
-			this.state.blips.push( { x: e.posx, y: e.posy, time: this.state.time } );
 	},
 	
 	//called when moving to other game
